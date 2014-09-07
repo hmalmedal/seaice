7fc07fecc0a746bf577af3e9fd5ef907efe27071
Håkon Malmedal <hmalmedal@gmail.com>
2014-09-07 17:59:00 +0200

    Align sliders in columns and add GitHub link

--- ui.R
+++ ui.R
@@ -7,10 +7,17 @@ shinyUI(fluidPage(
 
   ggvisOutput("ggvis"),
 
-  sliderInput(inputId = "percentage", label = "Inter-percentile range",
-              value = 50, min = 0, max = 100, step = 1),
+  hr(),
 
-  sliderInput(inputId = "years", label = "Years plotted",
-              value = 2, min = 1, max = 10, step = 1)
+  fluidRow(
+    column(width = 3,
+           sliderInput(inputId = "percentage",
+                       label = "Inter-percentile range",
+                       value = 50, min = 0, max = 100, step = 1)),
+    column(width = 3,
+           sliderInput(inputId = "years", label = "Years plotted",
+                       value = 2, min = 1, max = 10, step = 1))),
+
+  a(href = "https://github.com/hmalmedal/seaice", "GitHub")
 
 ))
