4cc2a86f1faf62b4c2f136ffb75b0d71372344f2
Håkon Malmedal <hmalmedal@gmail.com>
2015-07-27 20:37:36 +0200

    Drop function

--- seaice_approx.R
+++ seaice_approx.R
@@ -2,17 +2,16 @@ library(readr)
 library(stringr)
 library(dplyr)
 library(magrittr)
-read_seaice <- function(file)
-  read_csv(file) %>%
-  slice(-1) %>%
-  set_names(str_trim(names(.))) %>%
-  mutate_each(funs(str_trim)) %>%
-  mutate(Date = str_c(Year, Month, Day, sep = "-")) %>%
-  type_convert()
+
 NH_seaice_extent_final <- "ftp://sidads.colorado.edu/DATASETS/NOAA/G02135/north/daily/data/NH_seaice_extent_final.csv"
 NH_seaice_extent_nrt <- "ftp://sidads.colorado.edu/DATASETS/NOAA/G02135/north/daily/data/NH_seaice_extent_nrt.csv"
-NH_seaice_extent <- rbind(read_seaice(NH_seaice_extent_final),
-                          read_seaice(NH_seaice_extent_nrt))
+NH <- c(NH_seaice_extent_final, NH_seaice_extent_nrt)
+
+NH_seaice_extent <- lapply(NH, read_csv,
+                           col_names = c("Year", "Month", "Day", "Extent"),
+                           col_types = "nnnn__", skip = 2) %>%
+  bind_rows() %>%
+  mutate(Date = as.Date(str_c(Year, Month, Day, sep = "-")))
 
 seaice_approx <- NH_seaice_extent %$%
   approx(Date, Extent, seq(min(Date), max(Date), by = "days")) %$%
