3dc7e07498720005bae49168a201730091e558a0
Håkon Malmedal <hmalmedal@gmail.com>
2015-07-27 20:00:30 +0200

    Use sidebar layout

--- ui.R
+++ ui.R
@@ -1,25 +1,22 @@
 library(shiny)
 library(ggvis)
 
-shinyUI(fluidPage(
-
-  titlePanel("Arctic sea ice extent"),
-
-  ggvisOutput("ggvis"),
-
-  hr(),
-
-  fluidRow(
-    column(width = 3,
-           sliderInput(inputId = "percentage",
-                       label = "Inter-percentile range",
-                       value = 50, min = 0, max = 100, step = 1)),
-    column(width = 3,
-           sliderInput(inputId = "years", label = "Years plotted",
-                       value = 2, min = 1, max = 10, step = 1))),
-
-  fluidRow(column(width = 6, textOutput("explanation"))),
-
-  a(href = "https://github.com/hmalmedal/seaice", "GitHub")
-
-))
+shinyUI(
+  fluidPage(
+    titlePanel("Arctic sea ice extent"),
+    sidebarLayout(
+      sidebarPanel(
+        sliderInput(inputId = "percentage",
+                    label = "Inter-percentile range",
+                    value = 50, min = 0, max = 100, step = 1),
+        sliderInput(inputId = "years", label = "Years plotted",
+                    value = 2, min = 1, max = 10, step = 1),
+        textOutput("explanation"),
+        a(href = "https://github.com/hmalmedal/seaice", "GitHub")
+      ),
+      mainPanel(
+        ggvisOutput("ggvis")
+      )
+    )
+  )
+)
